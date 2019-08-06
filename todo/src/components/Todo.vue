be2734f5f9824c48390d8a3f075a37cb64e0ea12
Jeff Simons Decena <jeff.decena@yahoo.com>
2019-08-07 03:07:50 +0800

    Create the Vue list of Todos
    Signed-off-by: Jeff Simons Decena <jeff.decena@yahoo.com>

--- todo/src/components/Todo.vue
+++ todo/src/components/Todo.vue
@@ -1,14 +1,62 @@
 <template>
   <main class="container">
     <h1>My Simple Todo App</h1>
+    <form class="form-horizontal" v-on:submit.prevent="onSubmit()">
+      <div class="form-group">
+        <label for="todo" class="sr-only">What to do?</label>
+        <input v-model="todo" type="text" class="form-control" id="todo" placeholder="What to do today?">
+      </div>
+      <div id="listing">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Activity</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(list, idx) in lists.data" :key="idx">
+              <td>{{list.title}}</td>
+              <td>
+                <div class="btn-group">
+                  <button class="btn btn-success"><i class="fa fa-check"></i> Mark as done</button>
+                  <button class="btn btn-danger"><i class="fa fa-trash"></i>Delete todo</button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p v-if="lists.length === 0" class="alert alert-warning">Nothing to do yet, add something!</p>
+      </div>
+    </form>
   </main>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    data () {
+      return {
+        todo: null,
+        lists: []
+      }
+    },
+    created () {
+      this.listTodo()
+    },
+    methods: {
+      listTodo: function () {
+        return this.$http.get('http://127.0.0.1:8000/api/todo').then((response) => {
+          this.lists = response.data
+      })
+      },
+      onSubmit: function () {
+        this.$http.post('http://127.0.0.1:8000/api/todo', {
+          title: this.todo
+        }).then((response) => {
+          this.listTodo()
+          this.todo = null
+        })
+      }
+    }
   }
-}
 </script>
--- todo/src/main.js
+++ todo/src/main.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+library.add(faCoffee)
+Vue.use(VueAxios, axios)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
