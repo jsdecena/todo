605d23e78bfa5a3d931ec5093fde16faf2deab3a
Jeff Simons Decena <jeff.decena@yahoo.com>
2019-08-07 01:46:11 +0800

    Add the CORS MW
    Signed-off-by: Jeff Simons Decena <jeff.decena@yahoo.com>

---
+++ todo/src/components/Todo.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="container">
+    <h1>My Simple Todo App</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
